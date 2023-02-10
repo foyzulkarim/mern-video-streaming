f1ba8d53840b5af2cc5169990f66e11180cb8d70
Foyzul Karim <foyzulkarim@gmail.com>
2023-02-11 10:47:25 +1100

    learn-mern-raws-integrate-websocket-01

--- server/src/server.js
+++ server/src/server.js
@@ -10,18 +10,32 @@ const setup = async (db) => {
   setupRoutes(app);
 };
 
-app.listen(PORT, async () => {
-  console.log(`listening on port ${PORT}`);
-  const db = await connect("mongodb://localhost:27017");
-  await setup(db);
-  console.log("application setup completed");
-  // which request, what handler
-  app.use("/", (req, res) => {
-    console.log(`request received at ${new Date()}`);
-    console.log("req", req.body);
-    //console.dir(res);
-    res.send(`request received at ${new Date()}`);
-  });
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-  console.log("application started", new Date().toTimeString());
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.dir(socket.id);
 });
+
+server.listen(PORT, () => {
+  console.log("listening on *:4000");
+});
+
+// app.listen(PORT, async () => {
+//   console.log(`listening on port ${PORT}`);
+//   const db = await connect("mongodb://localhost:27017");
+//   await setup(db);
+//   console.log("application setup completed");
+//   // which request, what handler
+//   app.use("/", (req, res) => {
+//     console.log(`request received at ${new Date()}`);
+//     console.log("req", req.body);
+//     //console.dir(res);
+//     res.send(`request received at ${new Date()}`);
+//   });
+
+//   console.log("application started", new Date().toTimeString());
+// });
