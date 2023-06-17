1af21ff28712d2d8cc3a58c982313fa1e6677ec2
Foyzul Karim <foyzulkarim@gmail.com>
2023-06-17 21:04:30 +1000

    video list page show dynamic thumbnails and data from db

--- server/src/modules/models/video/handler.js
+++ server/src/modules/models/video/handler.js
@@ -7,6 +7,7 @@ const setup = () => {
   //   console.log('VIDEO_QUEUE_EVENTS.VIDEO_UPLOADED Event handler', data);
   // });
   console.log('registering video queue events');
+  const SERVER_URL = 'http://localhost:4000';
   Object.values(VIDEO_QUEUE_EVENTS).forEach((eventName) => {
     eventEmitter.on(eventName, async (data) => {
       console.log(`models/video/handler.js - ${eventName}`, data);
@@ -28,6 +29,7 @@ const setup = () => {
         await updateHistory(data.id, {
           history: { status: eventName, createdAt: new Date() },
           thumbnailPath: data.path,
+          thumbnailUrl: `${SERVER_URL}/thumbnails/${data.filename}.png`,
         });
         return;
       }
