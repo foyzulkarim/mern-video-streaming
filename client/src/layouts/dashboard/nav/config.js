b5e37c324cb61d24679b9a975b2f12a6734a5f08
fahim496 <fahimforhad496@gmail.com>
2023-06-30 18:22:42 +0600

    client side details page design updated. sidebar updated

--- client/src/layouts/dashboard/nav/config.js
+++ client/src/layouts/dashboard/nav/config.js
@@ -1,61 +1,61 @@
 // component
-import SvgColor from '../../../components/svg-color';
+import SvgColor from "../../../components/svg-color";
 
 // ----------------------------------------------------------------------
 
 const icon = (name) => (
-  <SvgColor
-    src={`/assets/icons/navbar/${name}.svg`}
-    sx={{ width: 1, height: 1 }}
-  />
+    <SvgColor
+        src={`/assets/icons/navbar/${name}.svg`}
+        sx={{ width: 1, height: 1 }}
+    />
 );
 
 const navConfig = [
-  {
-    title: 'dashboard',
-    path: '/dashboard/app',
-    icon: icon('ic_analytics'),
-  },
-  {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: icon('ic_user'),
-  },
-  {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: icon('ic_cart'),
-  },
-  {
-    title: 'videos',
-    path: '/videos',
-    icon: icon('ic_videos'),
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog'),
-  },
-  {
-    title: 'Video upload',
-    path: '/video-upload',
-    icon: icon('ic_blog'),
-  },
-  {
-    title: 'Video player',
-    path: '/video-player',
-    icon: icon('ic_blog'),
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
-  },
+    {
+        title: "dashboard",
+        path: "/dashboard/app",
+        icon: icon("ic_analytics"),
+    },
+    // {
+    //     title: "user",
+    //     path: "/dashboard/user",
+    //     icon: icon("ic_user"),
+    // },
+    // {
+    //     title: "product",
+    //     path: "/dashboard/products",
+    //     icon: icon("ic_cart"),
+    // },
+    {
+        title: "videos",
+        path: "/videos",
+        icon: icon("ic_videos"),
+    },
+    // {
+    //     title: "blog",
+    //     path: "/dashboard/blog",
+    //     icon: icon("ic_blog"),
+    // },
+    {
+        title: "Video upload",
+        path: "/video-upload",
+        icon: icon("ic_blog"),
+    },
+    {
+        title: "Video player",
+        path: "/video-player",
+        icon: icon("ic_blog"),
+    },
+    // {
+    //   title: 'login',
+    //   path: '/login',
+    //   icon: icon('ic_lock'),
+    // },
+    // {
+    //   title: 'Not found',
+    //   path: '/404',
+    //   icon: icon('ic_disabled'),
+    // },
 ];
 
 export default navConfig;
--- client/src/pages/VideoPlayerPage.js
+++ client/src/pages/VideoPlayerPage.js
@@ -1,89 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, useParams } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 // @mui
-import { styled } from '@mui/material/styles';
-import {
-  Container,
-  Stack,
-  TextField,
-  FormControl,
-  Typography,
-  Button,
-  Alert,
-  Snackbar,
-} from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import { LoadingButton } from '@mui/lab';
+import { styled } from "@mui/material/styles";
+import { Typography, Divider, Card, Box } from "@mui/material";
 
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import ReactPlayer from "react-player";
 
-import ReactPlayer from 'react-player';
+import axios from "axios";
 
-import axios from 'axios';
-
-const StyledContent = styled('div')(({ theme }) => ({
-  maxWidth: 600,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignContent: 'center',
-  alignItems: 'left',
-  flexDirection: 'column',
-  padding: theme.spacing(12, 0),
+const StyledContent = styled("div")(({ theme }) => ({
+    maxWidth: 800,
+    margin: "auto",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "top",
+    alignContent: "center",
+    alignItems: "left",
+    flexDirection: "column",
+    padding: theme.spacing(0, 0),
 }));
 
 export default function VideoUploadPage() {
-  const [url, setUrl] = useState('');
-  const { id } = useParams();
+    const [url, setUrl] = useState("");
+    const [data, setData] = useState("");
+    const { id } = useParams();
 
-  useEffect(() => {
-    console.log('id', id);
+    useEffect(() => {
+        console.log("id", id);
 
-    const fetchData = async () => {
-      const response = await axios.get(
-        `http://localhost:4000/api/videos/detail/${id}`
-      );
-      console.log(response);
-      const u = `http://localhost:4001/${response.data.fileName}.m3u8`;
-      setUrl(u);
-    };
+        const fetchData = async () => {
+            const response = await axios.get(
+                `http://localhost:4000/api/videos/detail/${id}`
+            );
+            // console.log(response);
+            setData(response.data);
+            // const videoDetails = await axios.get(
 
-    fetchData();
-  }, [id]);
+            // )
+            const u = `http://localhost:4001/${response.data.fileName}.m3u8`;
+            setUrl(u);
+        };
 
-  return (
-    <>
-      <Helmet>
-        <title> Video upload</title>
-      </Helmet>
+        fetchData();
+    }, [id]);
+    console.log(data);
 
-      <>
-        <Container>
-          <StyledContent>
-            <Typography variant='h4' sx={{ mb: 5 }}>
-              Upload video
-            </Typography>
-            <Stack>
-              <ReactPlayer
-                url={url}
-                controls
-                playing={false}
-                width='100%'
-                height='100%'
-                config={{
-                  file: { attributes: { controlsList: 'nodownload' } },
-                }}
-              />
-            </Stack>
-          </StyledContent>
-        </Container>
-      </>
-    </>
-  );
+    return (
+        <>
+            {/* <Helmet>
+                <title> Video upload</title>
+            </Helmet>
+
+            <>
+                <Container>
+                    <StyledContent>
+                        <Typography variant="h4" sx={{ mb: 5 }}>
+                            Upload video
+                        </Typography>
+                        <Stack>
+                            <ReactPlayer
+                                url={url}
+                                controls
+                                playing={false}
+                                width="100%"
+                                height="100%"
+                                config={{
+                                    file: {
+                                        attributes: {
+                                            controlsList: "nodownload",
+                                        },
+                                    },
+                                }}
+                            />
+                        </Stack>
+                    </StyledContent>
+                </Container>
+            </> */}
+
+            {/* <Stack> */}
+            <Helmet>
+                <title> Video upload</title>
+            </Helmet>
+            <StyledContent>
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                    {data?.title || "Upload video"}
+                </Typography>
+
+                <ReactPlayer
+                    url={url}
+                    controls
+                    playing={false}
+                    width="100%"
+                    height="100%"
+                    config={{
+                        file: {
+                            attributes: { controlsList: "nodownload" },
+                        },
+                    }}
+                />
+                <Box
+                    sx={{
+                        bgcolor: "#457dbc",
+                        boxShadow: 1,
+                        borderRadius: 2,
+                        p: 2,
+                        minWidth: 300,
+                    }}
+                >
+                    {"description "}
+                </Box>
+            </StyledContent>
+        </>
+    );
 }
