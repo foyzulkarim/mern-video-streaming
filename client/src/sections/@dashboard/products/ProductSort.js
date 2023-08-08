35ae8141b34a05f03a665c3e8489c7e887642b71
Jubair70 <jubair.hossain@yahoo.com>
2023-08-08 19:29:56 +0600

    decoupling product sort

--- client/src/sections/@dashboard/products/ProductSort.js
+++ client/src/sections/@dashboard/products/ProductSort.js
@@ -7,33 +7,21 @@ import Iconify from '../../../components/iconify';
 // ----------------------------------------------------------------------
 
 const SORT_BY_OPTIONS = [
-  { value: 'latest', label: 'Latest' },
-  { value: 'popular', label: 'Popular' },
-  { value: 'oldest', label: 'Oldest' },
+  { value: 'featured', label: 'Featured' },
+  { value: 'newest', label: 'Newest' },
+  { value: 'priceDesc', label: 'Price: High-Low' },
+  { value: 'priceAsc', label: 'Price: Low-High' },
 ];
 
-export default function ShopProductSort({setVideos }) {
+export default function ShopProductSort() {
   const [open, setOpen] = useState(null);
-  const [label, setLabel] = useState("Popular");
 
   const handleOpen = (event) => {
     setOpen(event.currentTarget);
   };
 
-  const handleClose = (event,val) => {
+  const handleClose = () => {
     setOpen(null);
-    if(val === 'backdropClick') return 
-    switch(val) {
-      case 'oldest':
-        setVideos(prev=>[...prev.sort((a, b) => new Date(a.recordingDate) - new Date(b.recordingDate))])
-        break;
-      case 'latest':
-        setVideos(prev=>[...prev.sort((a, b) => new Date(b.recordingDate) - new Date(a.recordingDate))])
-        break;
-      default:
-        setVideos(prev=>[...prev.sort((a, b) => b.viewCount - a.viewCount===0 ? new Date(b.recordingDate) - new Date(a.recordingDate) : b.viewCount - a.viewCount)])
-    }
-    setLabel(val)
   };
 
   return (
@@ -46,7 +34,7 @@ export default function ShopProductSort({setVideos }) {
       >
         Sort By:&nbsp;
         <Typography component="span" variant="subtitle2" sx={{ color: 'text.secondary' }}>
-          {label}
+          Newest
         </Typography>
       </Button>
       <Menu
@@ -60,8 +48,8 @@ export default function ShopProductSort({setVideos }) {
         {SORT_BY_OPTIONS.map((option) => (
           <MenuItem
             key={option.value}
-            selected={option.value === 'popular'}
-            onClick={(event)=>handleClose(event,option.value)}
+            selected={option.value === 'newest'}
+            onClick={handleClose}
             sx={{ typography: 'body2' }}
           >
             {option.label}
