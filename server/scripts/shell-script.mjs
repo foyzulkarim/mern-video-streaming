7e489a85af03e3636f8e12af07b961176f065520
aninda052 <anindahalder052@gmail.com>
2023-08-10 16:55:41 +0600

    fix: create uploads folders on mac (#57)

--- server/scripts/shell-script.mjs
+++ server/scripts/shell-script.mjs
@@ -7,6 +7,6 @@ var opsys = process.platform;
 
 if (opsys == "win32" || opsys == "win64") {
     await $`mkdir uploads/hls, uploads/processed, uploads/videos, uploads/thumbnails`
-} else if (opsys == "linux") {
+} else if (opsys == "linux" || opsys == "darwin") {
     await $`mkdir -p uploads/hls uploads/processed uploads/videos uploads/thumbnails`
 }
