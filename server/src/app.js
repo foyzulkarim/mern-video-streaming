07afd9b8ffabf3eb94c4ede0b8834d89ffedd83f
Foyzul Karim <foyzulkarim@gmail.com>
2023-09-18 13:24:14 +1000

    Implement `Winston` logging and send logs to console, loggly, file and MongoDB (#85)
    * logging work in progress
    
    * logging implemented
    
    * added conditional settings for loggly
    
    * Fixed logic

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const compression = require('compression');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
 app.use(compression());
 app.use(cors());
+
+const logger = require('./logger');
+
+const morganMiddleware = morgan(
+  ':method :url :status :res[content-length] - :response-time ms',
+  {
+    stream: {
+      // Configure Morgan to use our custom logger with the http severity
+      write: (message) => logger.http(message.trim()),
+    },
+  }
+);
+app.use(morganMiddleware);
 
 app.use('/thumbnails', express.static('./uploads/thumbnails'));
 
