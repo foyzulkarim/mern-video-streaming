7df6a7f3df8e961d2ee48015446d1448bcdfe73b
aninda052 <anindahalder052@gmail.com>
2023-10-01 14:27:03 +0600

    feat: (server) write http log using custom logger (#86)

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const compression = require('compression');
 const cors = require('cors');
-require('dotenv').config();
+const morgan = require('morgan');
+const logger = require('./logger');
 
+require('dotenv').config();
 
 require('elastic-apm-node').start({
   serviceName: 'mern-video-streaming',
@@ -12,11 +14,26 @@ require('elastic-apm-node').start({
   active: process.env.NODE_ENV === "production" || process.env.IS_APM_ACTIVE
 })
 
-
 const app = express();
 app.use(express.json());
 app.use(compression());
 app.use(cors());
+
+if(!process.env.IS_APM_ACTIVE){
+  const morganMiddleware = morgan(
+    ':method :url :status :res[content-length] - :response-time ms',
+    {
+      stream: {
+        // Configure Morgan to use our custom logger with the http severity
+        write: (message) => logger.http(message.trim()),
+      },
+    }
+  );
+  
+  app.use(morganMiddleware);
+}
+
+
 app.use('/thumbnails', express.static('./uploads/thumbnails'));
 
 module.exports = app;
